c708e522037f2644bbe30959b1d2719ca3e596c2
Laxmi-Artistic-Bird <laxmi@entraenlared.com>
2023-10-25 13:25:48 +0000

    new updates

--- src/public/cookies/cookie.js
+++ src/public/cookies/cookie.js
@@ -2,23 +2,7 @@ $(function(){
   var fileData;
   var url;
 
-  function checkCookie(cookieName) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.indexOf(cookieName + '=') === 0) {
-            // Cookie exists
-            return true;
-        }
-    }
-    // Cookie doesn't exist
-    return false;
-}
-
-var cookieName = 'consent';
-if (checkCookie(cookieName)) {
-  showbadge();
-}
+  
 
   checkDataFileOnServer();
   if (platformtype=='Laravel') {
